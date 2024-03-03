f6e37d375ac3340f3c46333136a8c732bd1ce164
ouozen <yakunin1996@gmail.com>
2024-03-03 08:41:00 +0000

    initial commit

---
+++ config_reader.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: SecretStr
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='ascii')
+
+config = Settings()
+print(config.BOT_TOKEN.get_secret_value())
