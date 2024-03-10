7b6f0b778d901306e26692e387c184ffe4055e33
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2024-03-10 18:38:13 +0800

    feat(bot): add a func to check your message by /getme

--- main.py
+++ main.py
@@ -29,6 +29,18 @@ async def cmd_start(message: types.Message):
     sys.exit(1)
 
 
+@dp.message(Command("getme"))
+async def cmd_get_my_messages(message: types.Message):
+    """
+    Get all your messages
+    :param message:
+    :return:
+    """
+    result = await fetch_data_by_user_id(message.from_user.id) or "Couldn't fetch user by id"
+    await message.answer(result)
+
+
+
 @dp.message(Command("chat_id"))
 async def cmd_chatid(message: types.Message):
     await message.answer(str(message.chat.id) + "_" + str(message.message_thread_id))
@@ -40,7 +52,7 @@ async def cmd_chatid(message: types.Message):
 
 
 # пересылает файлы из чата
-@dp.message((F.chat.id == -1001675679569) & (F.document))
+@dp.message((F.chat.id == -1001995048215) & (F.document))
 async def files(message: types.Message):
     ext_count = 0
     ext_list = ["gif", "mp3", "mp4", "png", "jpg", "jpeg", "txt", "json"]
@@ -49,20 +61,20 @@ async def files(message: types.Message):
             ext_count += 1
     if ext_count == 0:
         await message.react([react])
-        await bot.send_message(-1002027282215, message.from_user.username, message_thread_id=45)
-        await bot.send_document(-1002027282215, document=message.document.file_id, message_thread_id=45)
+        await bot.send_message(-1002121516896, message.from_user.username, message_thread_id=45)
+        await bot.send_document(-1002121516896, document=message.document.file_id, message_thread_id=45)
 
 
 # пересылает мемы из указанных групп
-@dp.message((F.chat.id == -1001675679569) & (F.forward_origin.chat.id.in_({-1001595506698, -1001081170974, -1001009232144, -1001399874898})))
+@dp.message((F.chat.id == -1001995048215) & (F.forward_origin.chat.id.in_({-1001595506698, -1001081170974, -1001009232144, -1001399874898})))
 async def memes(message: types.Message):
     await message.react([react])
-    await bot.forward_message(-1002027282215, message.chat.id, message.message_id, message_thread_id=44)
+    await bot.forward_message(-1002121516896, message.chat.id, message.message_id, message_thread_id=44)
 
 
 # пересылает вакансии в которых есть от 3 ключевых слов
-@dp.message((F.chat.id == -1001675679569) & (F.forward_origin))
-async def vacansies(message: types.Message):
+@dp.message((F.chat.id == -1001995048215) & (F.forward_origin))
+async def vacancies(message: types.Message):
     words_list = ["ищем", "вакансия", "junior", "middle", "senior", "компания", "зарплата", "задач", "python", "sql", "data", "аналитик", "ab", "a/b", "ml", "инженер"]
     words_found_count = 0
     
@@ -72,11 +84,11 @@ async def vacansies(message: types.Message):
         
     if words_found_count >= 3:
         await message.react([react])
-        await bot.forward_message(-1002027282215, message.chat.id, message.message_id, message_thread_id=2)
-
+        await bot.forward_message(-1002121516896, message.chat.id, message.message_id, message_thread_id=2)
+        await log_entry(message, 'vacancies')
 
 # пересылает статьи и курсы
-@dp.message((F.chat.id == -1001675679569) & (F.text))
+@dp.message((F.chat.id == -1001995048215) & (F.text))
 async def papers(message: types.Message):
     data = {
         "url": None,
@@ -87,12 +99,12 @@ async def papers(message: types.Message):
             data[item.type] = item.extract_from(message.text)
     if data["url"] is not None and "курс" not in str.lower(message.text):
         await message.react([react])
-        await bot.send_message(-1002027282215, message.from_user.username + ": " + data["url"], message_thread_id=3)
+        await bot.send_message(-1002121516896, message.from_user.username + ": " + data["url"], message_thread_id=3)
         await log_entry(message, 'articles')
 
     elif data["url"] is not None and "курс" in str.lower(message.text):
         await message.react([react])
-        await bot.send_message(-1002027282215, message.from_user.username + "\n" + message.text, message_thread_id=46)
+        await bot.send_message(-1002121516896, message.from_user.username + "\n" + message.text, message_thread_id=46)
         await log_entry(message, 'courses')
     else:
         await log_entry(message, 'trash')
