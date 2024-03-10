2d9a0cfb696f0672e802a59fce956765b8363589
Ouozen <yakunin1996@gmail.com>
2024-03-10 14:02:38 +0300

    issue Close #3

--- config_reader.py
+++ config_reader.py
@@ -4,7 +4,13 @@ from pydantic import SecretStr
 
 class Settings(BaseSettings):
     BOT_TOKEN: SecretStr
+    ORIGIN: SecretStr
+    ARCHIVE: SecretStr
+    VACANCIES: SecretStr
+    MEMES: SecretStr
+    FILES: SecretStr
+    PAPERS: SecretStr
+    COURSES: SecretStr
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='ascii')
 
 config = Settings()
-print(config.BOT_TOKEN.get_secret_value())
--- main.py
+++ main.py
@@ -5,13 +5,20 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from config_reader import config
 from aiogram import F
-from aiogram.types import InputFile
 
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.BOT_TOKEN.get_secret_value())
 dp = Dispatcher()
 react = types.ReactionTypeEmoji(emoji="✍")
+
+ORIGIN = int(config.ORIGIN.get_secret_value())
+ARCHIVE = int(config.ARCHIVE.get_secret_value())
+VACANCIES = int(config.VACANCIES.get_secret_value())
+MEMES = int(config.MEMES.get_secret_value())
+FILES = int(config.FILES.get_secret_value())
+PAPERS = int(config.PAPERS.get_secret_value())
+COURSES = int(config.COURSES.get_secret_value())
 
 
 @dp.message(Command("start"))
@@ -22,7 +29,7 @@ async def cmd_start(message: types.Message):
 @dp.message(Command("stop"))
 async def cmd_start(message: types.Message):
     await message.answer("I will be back ту-ту-ту-ту *музыка из терминатора*")
-    sys.exit(1)
+    raise SystemExit("stop")
 
 
 @dp.message(Command("chat_id"))
@@ -35,8 +42,8 @@ async def cmd_chatid(message: types.Message):
 #     print(message.model_dump_json())
 
 
-# пересылает файлы из чата
-@dp.message((F.chat.id == -1001675679569) & (F.document))
+# forwards files from the chat
+@dp.message((F.chat.id == ORIGIN) & (F.document))
 async def files(message: types.Message):
     ext_count = 0
     ext_list = ["gif", "mp3", "mp4", "png", "jpg", "jpeg", "txt", "json"]
@@ -45,19 +52,19 @@ async def files(message: types.Message):
             ext_count += 1
     if ext_count == 0:
         await message.react([react])
-        await bot.send_message(-1002027282215, message.from_user.username, message_thread_id=45)
-        await bot.send_document(-1002027282215, document=message.document.file_id, message_thread_id=45)
+        await bot.send_message(ARCHIVE, message.from_user.username, message_thread_id=FILES)
+        await bot.send_document(ARCHIVE, document=message.document.file_id, message_thread_id=FILES)
 
 
-# пересылает мемы из указанных групп
-@dp.message((F.chat.id == -1001675679569) & (F.forward_origin.chat.id.in_({-1001595506698, -1001081170974, -1001009232144, -1001399874898})))
+# forwards memes from the specified groups
+@dp.message((F.chat.id == ORIGIN) & (F.forward_origin.chat.id.in_({-1001595506698, -1001081170974, -1001009232144, -1001399874898})))
 async def memes(message: types.Message):
     await message.react([react])
-    await bot.forward_message(-1002027282215, message.chat.id, message.message_id, message_thread_id=44)
+    await bot.forward_message(ARCHIVE, message.chat.id, message.message_id, message_thread_id=MEMES)
 
 
-# пересылает вакансии в которых есть от 3 ключевых слов
-@dp.message((F.chat.id == -1001675679569) & (F.forward_origin))
+# forwards vacancies with at least 3 keywords
+@dp.message((F.chat.id == ORIGIN) & (F.forward_origin))
 async def vacansies(message: types.Message):
     words_list = ["ищем", "вакансия", "junior", "middle", "senior", "компания", "зарплата", "задач", "python", "sql", "data", "аналитик", "ab", "a/b", "ml", "инженер"]
     words_found_count = 0
@@ -68,11 +75,11 @@ async def vacansies(message: types.Message):
         
     if words_found_count >= 3:
         await message.react([react])
-        await bot.forward_message(-1002027282215, message.chat.id, message.message_id, message_thread_id=2)
+        await bot.forward_message(ARCHIVE, message.chat.id, message.message_id, message_thread_id=VACANCIES)
 
 
-# пересылает статьи и курсы
-@dp.message((F.chat.id == -1001675679569) & (F.text))
+# forwards articles and courses
+@dp.message((F.chat.id == ORIGIN) & (F.text))
 async def papers(message: types.Message):
     data = {
         "url": None,
@@ -83,14 +90,18 @@ async def papers(message: types.Message):
             data[item.type] = item.extract_from(message.text)
     if data["url"] is not None and "курс" not in str.lower(message.text):
         await message.react([react])
-        await bot.send_message(-1002027282215, message.from_user.username + ": " + data["url"], message_thread_id=3)
+        await bot.send_message(ARCHIVE, message.from_user.username + ": " + data["url"], message_thread_id=PAPERS)
     elif data["url"] is not None and "курс" in str.lower(message.text):
         await message.react([react])
-        await bot.send_message(-1002027282215, message.from_user.username + "\n" + message.text, message_thread_id=46)
+        await bot.send_message(ARCHIVE, message.from_user.username + "\n" + message.text, message_thread_id=COURSES)
 
 
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except SystemExit:
+        print("Shut down")
+        sys.exit(0)
