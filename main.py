fa1619131285affe8b3b3ba0b4840fc59e75782f
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2024-03-10 18:43:25 +0800

    feat(db): add func to init pg db and move chat_ids to const

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ bot = Bot(token=config.BOT_TOKEN.get_secret_value())
 dp = Dispatcher()
 react = types.ReactionTypeEmoji(emoji="✍")
 
+ORIGIN = -1001675679569
+ARCHIVE = -1002027282215
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
@@ -52,7 +54,7 @@ async def cmd_chatid(message: types.Message):
 
 
 # пересылает файлы из чата
-@dp.message((F.chat.id == -1001995048215) & (F.document))
+@dp.message((F.chat.id == ORIGIN) & (F.document))
 async def files(message: types.Message):
     ext_count = 0
     ext_list = ["gif", "mp3", "mp4", "png", "jpg", "jpeg", "txt", "json"]
@@ -61,19 +63,19 @@ async def files(message: types.Message):
             ext_count += 1
     if ext_count == 0:
         await message.react([react])
-        await bot.send_message(-1002121516896, message.from_user.username, message_thread_id=45)
-        await bot.send_document(-1002121516896, document=message.document.file_id, message_thread_id=45)
+        await bot.send_message(ARCHIVE, message.from_user.username, message_thread_id=45)
+        await bot.send_document(ARCHIVE, document=message.document.file_id, message_thread_id=45)
 
 
 # пересылает мемы из указанных групп
-@dp.message((F.chat.id == -1001995048215) & (F.forward_origin.chat.id.in_({-1001595506698, -1001081170974, -1001009232144, -1001399874898})))
+@dp.message((F.chat.id == ORIGIN) & (F.forward_origin.chat.id.in_({-1001595506698, -1001081170974, -1001009232144, -1001399874898})))
 async def memes(message: types.Message):
     await message.react([react])
-    await bot.forward_message(-1002121516896, message.chat.id, message.message_id, message_thread_id=44)
+    await bot.forward_message(ARCHIVE, message.chat.id, message.message_id, message_thread_id=44)
 
 
 # пересылает вакансии в которых есть от 3 ключевых слов
-@dp.message((F.chat.id == -1001995048215) & (F.forward_origin))
+@dp.message((F.chat.id == ORIGIN) & (F.forward_origin))
 async def vacancies(message: types.Message):
     words_list = ["ищем", "вакансия", "junior", "middle", "senior", "компания", "зарплата", "задач", "python", "sql", "data", "аналитик", "ab", "a/b", "ml", "инженер"]
     words_found_count = 0
@@ -84,11 +86,11 @@ async def vacancies(message: types.Message):
         
     if words_found_count >= 3:
         await message.react([react])
-        await bot.forward_message(-1002121516896, message.chat.id, message.message_id, message_thread_id=2)
+        await bot.forward_message(ARCHIVE, message.chat.id, message.message_id, message_thread_id=2)
         await log_entry(message, 'vacancies')
 
 # пересылает статьи и курсы
-@dp.message((F.chat.id == -1001995048215) & (F.text))
+@dp.message((F.chat.id == ORIGIN) & (F.text))
 async def papers(message: types.Message):
     data = {
         "url": None,
@@ -99,12 +101,12 @@ async def papers(message: types.Message):
             data[item.type] = item.extract_from(message.text)
     if data["url"] is not None and "курс" not in str.lower(message.text):
         await message.react([react])
-        await bot.send_message(-1002121516896, message.from_user.username + ": " + data["url"], message_thread_id=3)
+        await bot.send_message(ARCHIVE, message.from_user.username + ": " + data["url"], message_thread_id=3)
         await log_entry(message, 'articles')
 
     elif data["url"] is not None and "курс" in str.lower(message.text):
         await message.react([react])
-        await bot.send_message(-1002121516896, message.from_user.username + "\n" + message.text, message_thread_id=46)
+        await bot.send_message(ARCHIVE, message.from_user.username + "\n" + message.text, message_thread_id=46)
         await log_entry(message, 'courses')
     else:
         await log_entry(message, 'trash')
